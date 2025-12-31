dd22e72cca857206c56e7888fabd7ec10d579d68
malakus <malakus4@gmail.com>
2025-12-31 16:37:06 +0100

    new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Header.jsx'
-import Navbar from './navbar.jsx'
+// import Navbar from './navbar.jsx'
 import Barner from './Barner.jsx'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <Header/>
-    <Navbar/>
+    {/* <Navbar/> */}
     
     <Barner/>
 
