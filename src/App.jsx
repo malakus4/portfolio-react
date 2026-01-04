69ca6ac444e27d9af3126f1c49afb3be3cdcb222
malakus <malakus4@gmail.com>
2026-01-04 20:05:29 +0100

    feat: add Experiments component and route

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hometest from './pages/Hometest.jsx'
 import MainLayout from './layouts/MainLayout.jsx'
 import Home from './pages/Home.jsx'
 import Thoughts from './pages/Thoughts.jsx'
-import ExperimentPreview from './components/sections/ExperimentPreview.jsx'
+import Experiments from './pages/Experiments.jsx'
 
 function App() {
 
@@ -19,17 +19,7 @@ function App() {
         <Route path='/' 
           element={ <Home/> }/>
       </Route>
-     
-
-
-        {/* Test home page */}
-
-       {/* Main Layout route */}
-      <Route element={<MainLayout/>}>
-      <Route path='/hometest' element={<Hometest/>}/>
-      
-      </Route>
-
+    
 
 
       {/* route for Thoughts page */}
@@ -39,12 +29,15 @@ function App() {
           element={ <Thoughts/> }/>
       </Route>
 
+
       {/* route for Experiments page */}
-      {/* route for Work page */}
       <Route element={ <MainLayout/> }>
-        <Route path='/experiments' 
-          element={ <ExperimentPreview/> }/>
+        <Route path='/experiments'
+          element={ <Experiments/> }/>
       </Route>
+
+      {/* route for Work page */}
+      
     </Routes>
       
     </>
---
+++ src/pages/Experiments.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Experiments() {
+  return (
+    <div className='py-10'>Experiments page here</div>
+  )
+}
+
+export default Experiments
