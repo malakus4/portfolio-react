819aecf452ee4ac09bc5d158157f6a90b0c0d0a0
malakus <malakus4@gmail.com>
2026-01-06 23:11:02 +0100

    chore: update ThoughtSection contents

--- src/components/sections/ThoughtSection.jsx
+++ src/components/sections/ThoughtSection.jsx
@@ -6,38 +6,7 @@ function ThoughtSection() {
 
             <h4 className="thoughts_title title">Recent Thoughts</h4>
 
-            <div className="thoughts_textArea">
-
- {/* create 5 div, 2 para each  */}
-
-                <div className="thought">
-                    <p className="thought_date">8/15/2025</p>
-                    <p className="thought_title">Thoughts On Agentic Coding</p>
-
-                </div>
-
-                <div className="thought">
-                    <p className="thought_date">1/06/2024</p>
-                    <p className="thought_title">Learning to walk without a crutch</p>
-                </div>
-
-                <div className="thought">
-                    <p className="thought_date">12/28/2023</p>
-                    <p className="thought_title">2024</p>
-                </div>
-
-                 <div className="thought">
-                    <p className="thought_date">10/04/2022</p>
-                    <p className="thought_title">Interesting AI Articles & Links</p>
-                </div>
-
-                <div className="thought">
-                    <p className="thought_date">1/29/2022</p>
-                    <p className="thought_title">The "best" way to manage icons in React.js</p>
-                </div>
-
-
-            </div>
+           
 
         
             <div className="more_link"><a href="">More &rarr;</a></div>
