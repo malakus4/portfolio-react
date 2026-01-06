eb82f9418f76658020d385c497109f8aa381fe3a
malakus <malakus4@gmail.com>
2026-01-06 23:01:36 +0100

    chore: update About section text

--- src/components/sections/AboutSection.jsx
+++ src/components/sections/AboutSection.jsx
@@ -5,19 +5,7 @@ function AboutSection() {
     <div className='py-6'>
         <h2 className='py-2'>About Me</h2>
 
-        <div className='flex flex-col justify-center '>
-
-            <div className='flex flex-col justify-between items-center  aspect-video  bg-gray-200'>
-                <p className="pt-10 mt-10 ">
-                    Born. Didn't do too much that year.
-                </p>
-
-                <p>|I|I|I|I|I|I|I|I|I|I|I|I|I|I|I|I|I|I|I</p>
-            </div>
-
-            <p className='text-center py-2'>Scrub the timeline</p>
-
-        </div>
+        
 
     </div>
   )
