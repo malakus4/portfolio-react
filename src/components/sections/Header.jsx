b47daee136ef35184a56ffda201233aac2d59bf2
malakus <malakus4@gmail.com>
2026-01-06 22:50:55 +0100

    chore: update Header text

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 
 function Header() {
     return (
-        <section>
-            <h1 className='text-center py-10 text-2xl text-yellow-700'>Welcome to Tailwind in React</h1>
-        </section>
+        <header className="py-4 text-sm text-gray-500">
+            
+            {/* <p className="text-sm sm:text-base          lg:text-lg font-medium">Malak Udom - Frontend Developer </p> */}
+
+            <p className="text-[clamp(0.875rem,1vw,1.125rem)] font-medium">
+            Malak Udom &middot; Frontend Developer</p>
+
+
+        </header>
     )
 }
 
