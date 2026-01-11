5bde9f4f0c1867d28090532e254c4721acc05f0e
malakus <malakus4@gmail.com>
2026-01-11 21:55:15 +0100

    feat: add ContactCard to Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import FooterCopyright from './FooterCopyright'
 import FooterAbout from './FooterAbout'
 import FooterLinks from './FooterLinks'
+import FooterContactCard from './FooterContactCard'
 
 function Footer() {
   return (
@@ -14,12 +15,19 @@ function Footer() {
           {/* About + Links */}
           <div className="flex flex-row gap-8 justify-around lg:w-1/2">
             <FooterAbout/>
-            <FooterLinks/>
-
-          
-          
+            <FooterLinks/>    
           
           </div>
+
+          {/* Expandable cards */}
+          <div className="flex flex-col gap-6 lg:w-1/2 lg:flex-row">
+            <FooterContactCard/>
+
+            
+          </div>
+
+
+
         </div>
 
 
