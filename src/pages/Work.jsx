991f6303f2c1fd114ba21f5f730f3c44c3fa6648
malakus <malakus4@gmail.com>
2026-01-17 19:58:31 +0100

    feat: add BreadcrumbList to Work page

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
 function Work() {
+
+  const breadcrumbSchema = {
+  "@context": "https://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "name": "Home",
+      "item": "https://malakus-portfolio.vercel.app/"
+    },
+    {
+      "@type": "ListItem",
+      "position": 2,
+      "name": "Work",
+      "item": "https://malakus-portfolio.vercel.app/work"
+    }
+  ]
+};
+
   return (
-    <div className='container-page py-10'>This is work page</div>
+
+    <>
+    <script type="application/ld+json">
+          {JSON.stringify(breadcrumbSchema)}
+      </script>
+
+    <div className='container-page py-10'>
+      
+      <div>This is work page</div>
+     </div>
+
+    </>
+
   )
 }
-
 export default Work
