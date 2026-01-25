94b70bcfe8efbaa75c1f46baa9435bf817af6c67
malakus <malakus4@gmail.com>
2026-01-25 23:58:52 +0100

    style: comment out Header

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -7,7 +7,7 @@ import { Outlet } from 'react-router-dom'
 function MainLayout() {
   return (
     <>
-        <Header/>
+        {/* <Header/> */}
         <Nav />
 
         {/* Page contents renders here */}   
