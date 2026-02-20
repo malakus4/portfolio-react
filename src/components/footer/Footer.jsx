f7bea95d10f4dcca65ad55251fbcdeb4a06e1f89
malakus <malakus4@gmail.com>
2026-02-20 07:34:14 +0100

    refactor(footer): apply .container-page utility for consistent spacing

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,26 +1,21 @@
-import React from 'react'
-import FooterCopyright from './FooterCopyright'
-import FooterAbout from './FooterAbout'
-import FooterLinks from './FooterLinks'
-import FooterContactCard from './FooterContactCard'
-import FooterConnectCard from './FooterConnectCard'
+import FooterAbout from "./FooterAbout";
+import FooterLinks from "./FooterLinks";
+import FooterCopyright from "./FooterCopyright";
 
 function Footer() {
   return (
-
-    <footer className="bg-base-200 container-page py-16">
-
+    <footer className="bg-base-200  py-16">
       {/* About + Links */}
-      <div className=" max-w-5xl mx-auto px-4 py-12 grid gap-10 lg:grid-cols-3">            
-            <div className="col-span-1 lg:col-span-2">
-              <FooterAbout/>
-            </div>
-              <FooterLinks/>   
-          </div>        
+      <div className="container-page py-12 grid gap-10 lg:grid-cols-3">
+        <div className="col-span-1 lg:col-span-2">
+          <FooterAbout />
+        </div>
+        <FooterLinks />
+      </div>
 
-      <FooterCopyright/>
+      <FooterCopyright />
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
