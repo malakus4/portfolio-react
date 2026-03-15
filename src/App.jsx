35d1b5731f98af4e54333515e457cad5bb3873ac
malakus <malakus4@gmail.com>
2026-03-15 22:00:01 +0100

    feat: add ScrollToTop component for route navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.jsx";
 import Home from "./pages/Home.jsx";
 import Work from "./pages/Work.jsx";
@@ -6,10 +6,12 @@ import WorkDetail from "./pages/WorkDetail.jsx";
 import About from "./pages/About.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Contact from "./pages/Contact.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         {/* Main Layout route */}
 
